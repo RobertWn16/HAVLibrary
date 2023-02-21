06adbfeb245a894ac49c3c797cee08296faf18f5
robert,gheorghita <robert.gheorghita@prooptica.ro>
2023-02-21 19:16:43 +0200

    Update main for testing
    DEVELOPMENT

--- HAVLibrary/HAVLibrary/main.cpp
+++ HAVLibrary/HAVLibrary/main.cpp
@@ -139,9 +139,13 @@ winrt::com_ptr<ID3D11Device> pd3d11_dev;
 winrt::com_ptr<IDXGIFactory2> pdxgi_fty;
 winrt::com_ptr<IVideoSource> display_source;
 winrt::com_ptr<ID3D11DeviceContext> pd3d11_ctx;
-winrt::com_ptr<IHAV> hav_instance;
+winrt::com_ptr<IDXGIDevice3> pdxgi_dev;
+winrt::com_ptr<IDXGIAdapter> pdxgi_adapt;
+winrt::com_ptr<IDemuxer> demx;
+winrt::com_ptr<IDev> dev_nvidia;
 winrt::com_ptr<IPacket> pckt;
 winrt::com_ptr<IVideoOutput> vout;
+winrt::com_ptr<HAV> hav_instance = winrt::make_self<HAV>();
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -279,14 +283,20 @@ void DecodeMain(IDecoder* dec, THREAD_PARAMS& par, VIDEO_SOURCE_DESC vsrc_desc) 
 }
 void DecodeMainLoop(THREAD_PARAMS par)
 {
+
     winrt::com_ptr<IVideoSource> source;
     winrt::com_ptr<IDecoder> dec;
     winrt::com_ptr<IDecoder> nvjpeg_decoder;
     //cudaSetDeviceFlags(cudaDeviceScheduleBlockingSync);
     VIDEO_SOURCE_DESC vsrc_desc = { 0 };
     try {
+        DEV_DESC dv_desc;
+        dv_desc.vendor = NVIDIA;
+        dv_desc.ordinal = 0;
+        winrt::check_hresult(hav_instance->CreateDevice(IID_HAV_NVDev, dv_desc, dev_nvidia.put()));
         winrt::check_hresult(hav_instance->CreateDecoder(IID_HAV_NVDEC, dec.put()));
         winrt::check_hresult(hav_instance->CreateDecoder(IID_HAV_NVJpegDecoder, nvjpeg_decoder.put()));
+        winrt::check_hresult(hav_instance->CreateDemuxer(IID_HAV_FFMPEGDemuxer, demx.put()));
         winrt::check_hresult(demx->VideoCapture(par.name.c_str(), source.put()));
         winrt::check_hresult(hav_instance->Link(dev_nvidia.get(), dec.get()));
 
@@ -307,6 +317,97 @@ void DecodeMainLoop(THREAD_PARAMS par)
         std::cout << "Error 0x%x" << err.code();
         return;
     }
+}
+
+int main(int argc, char** argv)
+{
+    WNDCLASSEX wc;
+    HWND hwnd;
+    MSG Msg;
+
+    //Step 1: Registering the Window Class
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = 0;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = GetModuleHandle(NULL);
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = L"DLSSClass";
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+    if (!RegisterClassEx(&wc))
+    {
+        return 0;
+    }
+
+    // Step 2: Creating the Window
+    hwnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE,
+        L"DLSSClass",
+        L"The title of my window",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 1600, 763,
+        NULL, NULL, GetModuleHandle(NULL), NULL);
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    HWND core_hwnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE,
+        L"DLSSClass",
+        L"Second",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 1600, 763,
+        NULL, NULL, GetModuleHandle(NULL), NULL);
+
+    HRESULT hr = S_OK;
+    if (SUCCEEDED(hr)) hr = CreateDXGIFactory1(IID_IDXGIFactory3, (void**)&pdxgi_fty);
+
+    if (SUCCEEDED(hr)) hr = pdxgi_fty->EnumAdapters(0, (IDXGIAdapter**)pdxgi_adapt.put());
+
+    if (SUCCEEDED(hr))
+    {
+        hr = D3D11CreateDevice(
+            pdxgi_adapt.get(),
+            D3D_DRIVER_TYPE_UNKNOWN,
+            nullptr,
+            D3D11_CREATE_DEVICE_BGRA_SUPPORT,
+            nullptr,
+            0,
+            D3D11_SDK_VERSION,
+            pd3d11_dev.put(),
+            nullptr,
+            pd3d11_ctx.put()
+        );
+    }
+
+    ShowWindow(core_hwnd, SW_SHOWDEFAULT);
+    UpdateWindow(core_hwnd);
+
+    THREAD_PARAMS par;
+    par.name = "tcp://127.0.0.1:8080/live.h264";
+    par.hwnd = core_hwnd;
+    par.windowIsClosed = false;
+    HANDLE hThread = CreateThread(nullptr,
+        0,
+        reinterpret_cast<LPTHREAD_START_ROUTINE>(DecodeMainLoop),
+        &par,
+        0,
+        nullptr);
+
+    while (GetMessage(&Msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
+    }
+    WaitForSingleObject(hThread, INFINITE);
+
 }
 
 #endif // USE_HAV_ENCODE
