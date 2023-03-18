86bad9bd223a06d3e7b845c00f6315386229acd1
RobertWn16 <gheorghita_robert@outlook.com>
2023-03-18 20:22:27 +0200

    TEST
    TEST

--- HAVLibrary/HAVLibrary/Interfaces/IDecoder.hpp
+++ HAVLibrary/HAVLibrary/Interfaces/IDecoder.hpp
@@ -5,7 +5,7 @@
 // {E4EA258B-86C8-4528-9AFE-C1DCE01ACB1A}
 DEFINE_GUID(IID_HAV_IDecoder,
 	0xe4ea258b, 0x86c8, 0x4528, 0x9a, 0xfe, 0xc1, 0xdc, 0xe0, 0x1a, 0xcb, 0x1a);
-
+#define TEST_GIT
 struct DECODER_DESC
 {
 	unsigned int width;
